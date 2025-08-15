9a7705e2e390bdc667eaaa1b6a933d4dafc88bb0
Peter Csurgay <csurgay@gmail.com>
2025-08-15 23:07:42 +0200

    Jumps on/off of platforms

--- draw.js
+++ draw.js
@@ -8,43 +8,52 @@ document.addEventListener('keypress', (event) => {
         launchToPalya(event.key);
     }
 });
+let keepJumping = false;
 document.addEventListener('touchstart', (event) => {
-    jump();
+    keepJumping = true;
+});
+document.addEventListener('touchend', (event) => {
+    keepJumping = false;
 });
 const parabola = [1,4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361,400];
 const parLength = 17;
 const w = 1400;
 const h = 600;
+const hBase = 3 * h / 4; // height of the base line
+const xJatekos = Math.floor(w / 4); // x position of the player
+const dVonal = Math.floor(w / 4); // distance between vertical lines
 const speed = 10; // speed of the game
 let step = 0;
 let spawn = 0;
-let vonal = { x: w/5 };
-let elemek = {
-    "cs1": {tipus: "csapda", h: 1 },
-    "cs2": {tipus: "csapda", h: 0.6 },
-    "o1": {tipus: "oszlop", h: 1 },
-    "o2": {tipus: "oszlop", h: 2 },
-    "o3": {tipus: "oszlop", h: 3 },
-};
+let vonal = { x: dVonal };
 const d3 = 90; // diameter for triangle
 const d4 = 60; // diameter for player square
 const d4r = Math.sqrt(2)/2 * d4; // radius for player square
+let elemek = {
+    "cs1": {tipus: "csapda", h: 1.2*d4 },
+    "cs2": {tipus: "csapda", h: 0.7*d4 },
+    "o1": {tipus: "oszlop", h: 1*d4 },
+    "o2": {tipus: "oszlop", h: 2*d4 },
+    "o3": {tipus: "oszlop", h: 3*d4 },
+};
+const dd = 2.5 * d4;
 let combos = [
     ["cs1", 0],
     ["cs1", 0, "cs2", d3],
     ["o1", 0],
     ["cs1", 0, "o1", d3],
-    ["o1", 0, "cs1", 2*d4, "o2", 4*d4, "cs1", 6*d4, "o3", 8*d4],
+    ["o1", 0, "cs1", dd, "o2", 2*dd, "cs1", 3*dd, "o3", 4*dd],
     ["o1", 0, "o1", d4, "o1", 2*d4, "o1", 3*d4],
     ["o1", 0, "o1", d4, "o1", 2*d4, "o1", 3*d4, "o1", 4*d4, "o1", 5*d4, "o1", 6*d4],
 ];
-let palya = [];
-palya.push({ x: w + 50, elem: elemek["cs1"] });
-let jatekos = { y: 3*h/4 - d4/2, angle: Math.PI / 4, state: "normal", r: d4r, phase: 0 };
+let palya = []; // items: x and elem
+let itemAtJatekos = null;
+palya.push({ x: w + 150, elem: elemek["cs1"] });
+let jatekos = { y: hBase - d4/2, angle: Math.PI / 4, state: "idle", r: d4r, phase: 0 }; // "normal", "jump", "fall", "dead"
 
 function jump() {
-    if (jatekos.state === "normal") {
-        jatekos.state = "jumps-up";
+    if (jatekos.state === "idle") {
+        jatekos.state = "jump";
         jatekos.phase = 0;
     }
 }
@@ -56,7 +65,7 @@ function draw() {
     // Új pálya elemek
     drawPalya();
     if (step - spawn > 500) {
-        if(Math.random() < 0.01) {
+        if(Math.random() < 0.03) {
             let comboRandom = Math.floor(combos.length * Math.random());
             launchToPalya(comboRandom);
         }
@@ -65,40 +74,64 @@ function draw() {
     // játékos
     drawJatekos();
 
-    for (let index = 0; index < palya.length; index++) {
-        let item = palya[index];
-        if (item.x > w / 4 && item.x < w / 4 + d4 && jatekos.y > 3*h/4 - item.elem.h * d4 && jatekos.y < 3*h/4) {
-            // hit detection
-            jatekos.state = "death";
-        }
+    // death detection
+    if (itemAtJatekos && 
+        //itemAtJatekos.elem.tipus == "csapda" && 
+        kozte(itemAtJatekos.x, xJatekos, xJatekos+d4) && 
+        kozte(jatekos.y, hBase-itemAtJatekos.elem.h-d4/3, hBase-d4/2)) 
+    {
+        jatekos.state = "dead";
     }
 
-    if (jatekos.state == "jumps-up") {
+    if (jatekos.state == "jump") {
         jatekos.angle += Math.PI / 2 / (parLength-1); // Rotate the player
         jatekos.y -= Math.floor(parabola[parLength-1-jatekos.phase] * 0.1);
         jatekos.phase ++;
         if (jatekos.phase >= parLength - 1) {
-            jatekos.state = "jumps-down";
+            jatekos.state = "fall";
         }
     }
-    if (jatekos.state == "jumps-down") {
+    if (jatekos.state == "fall") {
         jatekos.angle += Math.PI / 2 / (parLength); // Rotate the player
         jatekos.y += Math.floor(parabola[parLength-1-jatekos.phase] * 0.1);
         jatekos.phase --;
         if (jatekos.phase < 0) {
-            jatekos.state = "normal";
-        }        
+            jatekos.state = "idle";
+        }
+        else {
+            // check if player falls on platform
+            if (itemAtJatekos && 
+                itemAtJatekos.elem.tipus == "oszlop" && 
+                kozte(itemAtJatekos.x, xJatekos - d4, xJatekos + 2 * d4) &&
+                kozte(jatekos.y + d4/2, hBase - itemAtJatekos.elem.h - d4/4, hBase - itemAtJatekos.elem.h))
+            {
+                jatekos.y = hBase - itemAtJatekos.elem.h - d4/2; // reset player position
+                jatekos.state = "idle"; // reset player state
+                jatekos.angle = Math.PI / 4; // reset player angle
+            }
+        }
     }
-    if (jatekos.state == "death") {
+    // fall detection
+    if (jatekos.state == "idle") {
+        let whereToFall = hBase - d4 / 2;
+        if (itemAtJatekos) whereToFall = itemAtJatekos.h - d4 / 2;
+        if (jatekos.y < whereToFall) jatekos.y = whereToFall;
+    }
+
+    if (jatekos.state == "dead") {
         jatekos.angle += Math.PI / 2 / (parLength); // Rotate the player
         jatekos.r--; // shrink the player
         if (jatekos.r <= 0) {
-            jatekos.state = "normal";
-            jatekos.y = 3*h/4 - d4/2; // reset player position
+            jatekos.state = "idle";
+            jatekos.y = hBase - d4/2; // reset player position
             jatekos.r = d4r; // reset player size
             jatekos.angle = Math.PI / 4; // reset player angle
             palya = []; // reset the game
         }        
+    }
+
+    if (jatekos.state == "idle" && keepJumping) {
+        jump();
     }
 
     requestAnimationFrame(draw);
@@ -106,12 +139,16 @@ function draw() {
 
 draw();
 
-function launchToPalya(comboIndex) {
+function kozte(a, b, c) {
+    return a >= b && a <= c;
+}
+
+ function launchToPalya(comboIndex) {
     let combo = combos[comboIndex];
     for (let i=0; i<combo.length; i+=2) {
         let elemName = combo[i];
         let offset = combo[i+1];
-        palya.push( { x: w + 50 + offset, elem: elemek[elemName] } );
+        palya.push( { x: w + 150 + offset, elem: elemek[elemName] } );
     }
     spawn = step;
 }
@@ -119,27 +156,27 @@ function launchToPalya(comboIndex) {
 function drawHatter() {
     ctx.beginPath();
     ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, w, 3*h/4);
+    ctx.fillRect(0, 0, w, hBase);
     ctx.fill();
     ctx.beginPath();
     ctx.fillStyle = "darkblue";
-    ctx.fillRect(0, 3*h/4, w, h);
+    ctx.fillRect(0, hBase, w, h);
     ctx.fill();
 
     // base line
     ctx.beginPath();
     ctx.strokeStyle = "lightblue";
     ctx.lineWidth = 10;
-    ctx.moveTo(0, 3*h/4);
-    ctx.lineTo(w, 3*h/4);
+    ctx.moveTo(0, hBase);
+    ctx.lineTo(w, hBase);
     // vertical lines
-    for (let x = vonal.x; x < w + 50; x += Math.floor(w/5)) {
-        ctx.moveTo(x, 3*h/4);
+    for (let x = vonal.x; x < w + 150; x += dVonal) {
+        ctx.moveTo(x, hBase);
         ctx.lineTo(x, h);
     }
     ctx.stroke();
     if (vonal.x < speed) {
-        vonal.x = Math.floor(w/5);
+        vonal.x = dVonal;
     }
     vonal.x -= speed;
 }
@@ -149,20 +186,24 @@ function drawPalya() {
     ctx.fillStyle = "black";
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
+    itemAtJatekos = null;
     for (let index=0; index<palya.length; index++) {
         let item = palya[index];
         let elem = item.elem;
         if (elem.tipus == "csapda") {
-            let dh = elem.h * d3;
-            ctx.moveTo(item.x, 3*h/4 - dh * Math.sqrt(3)/2);
-            ctx.lineTo(item.x + d3*3/4 / 2, 3*h/4);
-            ctx.lineTo(item.x - d3*3/4 / 2, 3*h/4);
-            ctx.lineTo(item.x, 3*h/4 - dh * Math.sqrt(3)/2);
+            let dh = elem.h;
+            ctx.moveTo(item.x, hBase - dh);
+            ctx.lineTo(item.x + d3*3/4 / 2, hBase);
+            ctx.lineTo(item.x - d3*3/4 / 2, hBase);
+            ctx.lineTo(item.x, hBase - dh);
         }
         else if (elem.tipus == "oszlop") {
-            ctx.rect(item.x - d4/2, 3*h/4 - elem.h * d4, d4, elem.h * d4);
+            ctx.rect(item.x - d4/2, hBase - elem.h, d4, elem.h);
         }
         item.x -= speed;
+        if (kozte(item.x, xJatekos- d4, xJatekos + 2 * d4)) {
+            itemAtJatekos = item;
+        }
         ctx.font = "20px Arial";
 //        ctx.fillText(item.elem.tipus, item.x, 3*h/4 - elem.h * d4 - 10);
         if (item.x < -150) {
@@ -178,7 +219,7 @@ function drawJatekos() {
     ctx.strokeStyle = "green";
     ctx.lineWidth = 1;
     ctx.fillStyle = "green";
-    let x = w / 4;
+    let x = xJatekos;
     let y = jatekos.y;
     let r = jatekos.r;
     let a = jatekos.angle
